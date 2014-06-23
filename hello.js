af042c557577ad7eae75d703f42b3f9760baede2
Avi Haiat <thaiat@ipelia.com>
2014-06-23 15:26:46 +0300

    add another doc

--- hello.js
+++ hello.js
@@ -7,4 +7,5 @@ http.createServer(function (req, res) {
     res.end('Hello World\n'); // missing semi-colon will fail the build
 }).listen(1337, '127.0.0.1'); // reporting to the console
 
+// feedback to the user
 console.log('Server running at https://127.0.0.1:1337');
