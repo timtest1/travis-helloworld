e436f060e51efb38e97425c1c1060dd8eb39e7c8
Avi Haiat <thaiat@ipelia.fr>
2014-06-23 22:36:10 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
         'Content-Type': 'text/plain'
     });
     res.end('Hello World\n'); // missing semi-colon will fail the build
-}).listen(1337, '127.0.0.1'); // reporting to the console
+}).listen(1337, '127.0.0.1') // reporting to the console
 
 // feedback to the user
 console.log('Server running at https://127.0.0.1:1337');
