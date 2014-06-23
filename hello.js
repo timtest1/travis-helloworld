2fd6fd403d319339fbb081591d1a238850faf41b
Avi Haiat <thaiat@ipelia.com>
2014-06-23 15:07:58 +0300

    added semi colon to pass the build

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
-    res.end('Hello World\n') // missing semi-colon will fail the build
+    res.end('Hello World\n'); // missing semi-colon will fail the build
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at https://127.0.0.1:1337');
